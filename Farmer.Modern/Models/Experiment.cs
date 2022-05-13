bb65c4d2078bd5260fd76b1fee48d184ec9a4d77
Masoud Sharifi <m.sharifipoor@alibaba.ir>
2022-05-13 09:53:05 +0430

    Update model experiment

--- Farmer.Modern/Models/Experiment.cs
+++ Farmer.Modern/Models/Experiment.cs
@@ -1,10 +1,13 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace Farmer.Modern.Models;
 
 public class Experiment
 {
-    public long Id { get; set; }
-    public Garden Garden { get; set; }
-    public WaterMotor WaterMotor { get; set; }
-    public string Description { get; set; }
-    public string Result { get; set; }
+    [Key] public long Id { get; set; }
+    [DisplayName("نام زمین")] public Garden Garden { get; set; }
+    [DisplayName("نام موتور")] public WaterMotor WaterMotor { get; set; }
+    [DisplayName("نتیجه")] public string Result { get; set; }
+    [DisplayName("توضیحات")] public string Description { get; set; }
 }
