dc0a77947b687051140bf60c884a47af138ba011
Masoud Sharifi <m.sharifipoor@alibaba.ir>
2022-05-19 22:59:59 +0430

    Update ui index

--- Farmer.Modern/Controllers/HomeController.cs
+++ Farmer.Modern/Controllers/HomeController.cs
@@ -1,40 +1,63 @@
-﻿using System.Diagnostics;
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 using System.Threading.Tasks;
+using Farmer.Modern.Dto;
 using Microsoft.AspNetCore.Mvc;
 using Farmer.Modern.Models;
+using Farmer.Modern.Models.DbContext;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace Farmer.Modern.Controllers
 {
-public class HomeController : Controller
-{
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
+        private readonly ILogger<HomeController> _logger;
+        private readonly ApplicationDbContext _dbContext;
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext dbContext)
+        {
+            _logger = logger;
+            _dbContext = dbContext;
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public async Task<IActionResult> Index()
+        {
+            var homeDto = new HomeDto();
+            var values = await GetCountWorkStatus();
+            homeDto.WorkCount = values;
+            return View(homeDto);
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
-    }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
 
-    public async Task AddRole(string roleName)
-    {
-        await Task.FromResult(roleName);
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+
+        public async Task AddRole(string roleName)
+        {
+            await Task.FromResult(roleName);
+        }
+
+        private async Task<WorkCount> GetCountWorkStatus()
+        {
+            var dic = new WorkCount();
+            var results = _dbContext.Work.AsQueryable();
+
+            dic.All = await results.CountAsync();
+            dic.Block = await results.CountAsync(x => x.Status == ActionStatus.Block);
+            dic.Done = await results.CountAsync(x => x.Status == ActionStatus.Done);
+            dic.Todo = await results.CountAsync(x => x.Status == ActionStatus.Todo);
+            dic.InProgress = await results.CountAsync(x => x.Status == ActionStatus.InProgress);
+
+            return dic;
+        }
     }
 }
-}
-
---
+++ Farmer.Modern/Dto/HomeDto.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace Farmer.Modern.Dto;
+
+public class HomeDto
+{
+    public WorkCount WorkCount { get; set; }
+}
+
+public class WorkCount
+{
+    public int Todo { get; set; }
+    public int InProgress { get; set; }
+    public int Block { get; set; }
+    public int Done { get; set; }
+
+    public int All { get; set; }
+}
